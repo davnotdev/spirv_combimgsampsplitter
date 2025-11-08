abc9edffb3b5b8aeed6c600fa851dd855744d4b7
davnotdev <usr21p1@gmail.com>
2025-11-08 14:45:11 -0800

    Reverse my backwards logic

--- src/splitdref.rs
+++ src/splitdref.rs
@@ -1,26 +1,10 @@
 use super::*;
 
-//  DREF OPS:
-//
-//  OpImageSampleDrefImplicitLod
-//  OpImageSampleDrefExplicitLod
-//  OpImageSampleProjDrefImplicitLod
-//  OpImageSampleProjDrefExplicitLod
-//  OpImageDrefGather
-//  OpImageSparseSampleDrefImplicitLod
-//  OpImageSparseSampleDrefExplicitLod
-//  OpImageSparseDrefGather
-//
-//  - OpImageSampleProjDrefImplicitLod %result %sampled ...
-//      - backtrace OpSampledImage %type %sampled %loaded_image
-//          - backtrace OpLoad %type %loaded_image %image
-//              - return %image
-
-// ```
-// bump OpDecorate %new_image
-// duplicate OpVariable %image for %new_image
-// replace OpLoad's %image with %new_image
-// ```
+#[derive(Debug, Clone, Copy)]
+enum OperationVariant {
+    Regular,
+    Dref,
+}
 
 /// Perform the operation on a `Vec<u32>`.
 /// Use [u8_slice_to_u32_vec] to convert a `&[u8]` into a `Vec<u32>`
@@ -57,7 +41,7 @@ pub fn dreftexturesplitter(in_spv: &[u32]) -> Result<Vec<u32>, ()> {
         let instruction = loword(op);
 
         match instruction {
-            SPV_INSTRUCTION_OP_TYPE_SAMPLED_IMAGE => op_sampled_image_idxs.push(spv_idx),
+            SPV_INSTRUCTION_OP_SAMPLED_IMAGE => op_sampled_image_idxs.push(spv_idx),
             SPV_INSTRUCTION_OP_LOAD => op_load_idxs.push(spv_idx),
             SPV_INSTRUCTION_OP_IMAGE_SAMPLE_DREF_IMPLICIT_LOD
             | SPV_INSTRUCTION_OP_IMAGE_SAMPLE_DREF_EXPLICIT_LOD
@@ -88,58 +72,64 @@ pub fn dreftexturesplitter(in_spv: &[u32]) -> Result<Vec<u32>, ()> {
         spv_idx += word_count as usize;
     }
 
-    // 2. Find sampled images that mix operations
-    let mut sampled_image_mixing_status: HashMap<u32, (bool, bool)> = HashMap::new();
-
-    for idx in op_sampled_operation_idxs {
-        // Conveniently, the offset for this value is always +3.
-        let loaded_sampled_image_id = spv[idx + 3];
-        let entry = sampled_image_mixing_status
-            .entry(loaded_sampled_image_id)
-            .or_insert((false, false));
-
-        entry.0 = true;
-    }
-    for idx in op_dref_operation_idxs {
-        // Conveniently, the offset for this value is always +3.
-        let loaded_sampled_image_id = spv[idx + 3];
-        let entry = sampled_image_mixing_status
-            .entry(loaded_sampled_image_id)
-            .or_insert((false, false));
-
-        entry.1 = true;
-    }
-
-    let mixed_loaded_sampled_image_ids = sampled_image_mixing_status
-        .into_iter()
-        .filter_map(|(id, (uses_regular, uses_dref))| (uses_regular && uses_dref).then_some(id))
+    // 2. Collect all the loaded sampled images of both operation types
+    // Conveniently, the offset for this value is always +3 for all of these operations
+    let loaded_sampled_image_ids = op_sampled_operation_idxs
+        .iter()
+        .map(|idx| (spv[idx + 3], OperationVariant::Regular))
+        .chain(
+            op_dref_operation_idxs
+                .iter()
+                .map(|idx| (spv[idx + 3], OperationVariant::Dref)),
+        )
         .collect::<Vec<_>>();
 
-    // 3. Backtrack to find the OpSampledImage that resulted in our loaded sampled images
-    let mixed_loaded_image_ids = op_sampled_image_idxs
+    // 3. Backtrace to find the OpSampledImage that resulted in our loaded sampled images
+    let loaded_image_ids = op_sampled_image_idxs
         .iter()
         .filter_map(|idx| {
-            let sampled_result_id = spv[idx + 3];
-            let loaded_image_id = spv[idx + 4];
-            mixed_loaded_sampled_image_ids
-                .contains(&sampled_result_id)
-                .then_some(loaded_image_id)
+            let sampled_result_id = spv[idx + 2];
+            let loaded_image_id = spv[idx + 3];
+            loaded_sampled_image_ids
+                .iter()
+                .find_map(|(id, ty)| (*id == sampled_result_id).then_some(ty))
+                .map(|ty| (loaded_image_id, ty))
         })
         .collect::<Vec<_>>();
 
     // 4. Backtrack to find the OpLoad that resulted in our loaded images
-    let mixed_image_ids = op_load_idxs
+    let image_ids = op_load_idxs
         .iter()
         .filter_map(|idx| {
             let loaded_result_id = spv[idx + 2];
             let original_image = spv[idx + 3];
-            mixed_loaded_image_ids
-                .contains(&loaded_result_id)
-                .then_some((original_image, idx))
+            loaded_image_ids
+                .iter()
+                .find_map(|(id, ty)| (*id == loaded_result_id).then_some(ty))
+                .map(|ty| (original_image, idx, ty))
         })
         .collect::<Vec<_>>();
 
-    // 5. Duplicate OpVariable with a new_id and patch old OpLoad
+    // 5. Find the images that mix operations
+    let mut mixed_image_ids = HashMap::new();
+    let mut image_id_to_loads = HashMap::new();
+
+    for (id, load_idx, ty) in image_ids {
+        let entry = mixed_image_ids.entry(id).or_insert((false, false));
+        image_id_to_loads.entry(id).or_insert(vec![]).push(load_idx);
+
+        match ty {
+            OperationVariant::Regular => entry.0 = true,
+            OperationVariant::Dref => entry.1 = true,
+        }
+    }
+
+    let mixed_image_ids = mixed_image_ids
+        .into_iter()
+        .filter_map(|(id, (uses_regular, uses_dref))| (uses_regular && uses_dref).then_some(id))
+        .collect::<Vec<_>>();
+
+    // 5. Duplicate OpVariable with a new_id and patch old OpLoads
     // NOTE: GENERALLY, with glslc, each OpImage* will get its own OpLoad, so we don't need to
     // check that its result isn't used for both regular and dref operations!
     let patch_variable_idxs = op_variable_idxs
@@ -148,38 +138,42 @@ pub fn dreftexturesplitter(in_spv: &[u32]) -> Result<Vec<u32>, ()> {
             let result_id = spv[idx + 2];
             mixed_image_ids
                 .iter()
-                .find_map(|(id, op_load_idx)| (*id == result_id).then_some(*op_load_idx))
-                .map(|op_load_idx| (idx, op_load_idx))
+                .find(|id| **id == result_id)
+                .map(|id| (idx, id))
         })
         .collect::<Vec<_>>();
 
     let mut affected_variables = Vec::new();
 
-    for (&variable_idx, &op_load_idx) in patch_variable_idxs {
+    for (&variable_idx, &old_variable_id) in patch_variable_idxs {
         let word_count = hiword(spv[variable_idx]);
 
         // OpVariable
         let new_variable_id = instruction_bound;
         instruction_bound += 1;
         let mut new_variable = Vec::new();
-        new_variable.copy_from_slice(&spv[variable_idx..variable_idx + word_count as usize]);
+        new_variable.extend_from_slice(&spv[variable_idx..variable_idx + word_count as usize]);
         new_variable[2] = new_variable_id;
         instruction_inserts.push(InstructionInsert {
             previous_spv_idx: variable_idx,
             instruction: new_variable,
         });
 
-        // OpLoad
-        word_inserts.push(WordInsert {
-            idx: op_load_idx + 3,
-            word: new_variable_id,
-            head_idx: op_load_idx,
-        });
-
         affected_variables.push(util::DecorationVariable {
             original_res_id: spv[variable_idx + 2],
             new_res_id: new_variable_id,
         });
+
+        // OpLoad
+        if let Some(op_load_idxs) = image_id_to_loads.get(&old_variable_id) {
+            for &op_load_idx in op_load_idxs {
+                word_inserts.push(WordInsert {
+                    idx: op_load_idx + 3,
+                    word: new_variable_id,
+                    head_idx: *op_load_idx,
+                });
+            }
+        }
     }
 
     // 6. Insert new OpDecorate
