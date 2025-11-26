b7a9c197840f2b470e29bfeeaa512466031a3cae
davnotdev <usr21p1@gmail.com>
2025-11-26 01:04:31 -0800

    Remove what seems to be an outdated workaround

--- src/util/correct_decorate.rs
+++ src/util/correct_decorate.rs
@@ -54,24 +54,13 @@ pub fn correct_decorate(cd_in: CorrectDecorateIn) {
         // We can assume that our new ~~samplers~~ variables will have a greater instruction ID than the original
         // ~~combined image samplers~~ variables.
         let mut prev_binding = -1;
-        let mut prev_id = -1;
-        let mut prev_d_idx = -1;
         let mut increment = 0;
         for (d_idx, binding) in bindings {
-            let this_id = new_spv[d_idx + 1];
-
             if binding as i32 == prev_binding {
                 increment += 1;
-
-                if prev_id <= this_id as i32 {
-                    new_spv[prev_d_idx as usize + 3] += 1;
-                    new_spv[d_idx + 3] -= 1;
-                }
             }
-            new_spv[d_idx + 3] += increment;
+            new_spv[d_idx + 3] = binding + increment;
             prev_binding = binding as i32;
-            prev_id = this_id as i32;
-            prev_d_idx = d_idx as isize;
         }
     }
 }
