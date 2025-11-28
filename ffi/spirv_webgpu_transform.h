6a22fcd6401ed7b55e0fa6fa907b9d63750f77a1
davnotdev <usr21p1@gmail.com>
2025-11-27 17:28:55 -0800

    WIP updating ffi

--- ffi/spirv_webgpu_transform.h
+++ ffi/spirv_webgpu_transform.h
@@ -1,13 +1,23 @@
-#ifndef COMBIMGSAMPSPLITTER_H
-#define COMBIMGSAMPSPLITTER_H
+#ifndef SPIRV_WEBGPU_TRANSFORM_H
+#define SPIRV_WEBGPU_TRANSFORM_H
 
 #include <stdint.h>
 
 extern "C" {
-    void combimgsampsplitter_alloc(uint32_t* in_spv, uint32_t in_count, uint32_t** out_spv, uint32_t* out_count);
-    void combimgsampsplitter_free(uint32_t* out_spv);
-    void dreftexturesplitter_alloc(uint32_t* in_spv, uint32_t in_count, uint32_t** out_spv, uint32_t* out_count);
-    void dreftexturesplitter_free(uint32_t* out_spv);
+
+typedef void* TransformCorrectionMap;
+
+#define SPIRV_WEBGPU_TRANFORM_CORRECTION_MAP_NULL NULL
+
+void spirv_webgpu_transform_combimgsampsplitter_alloc(uint32_t* in_spv, uint32_t in_count, uint32_t** out_spv, uint32_t* out_count, TransformCorrectionMap* correction_map);
+void spirv_webgpu_transform_combimgsampsplitter_free(uint32_t* out_spv);
+void spirv_webgpu_transform_drefsplitter_alloc(uint32_t* in_spv, uint32_t in_count, uint32_t** out_spv, uint32_t* out_count, TransformCorrectionMap* correction_map);
+void spirv_webgpu_transform_drefsplitter_free(uint32_t* out_spv);
+
+void spirv_webgpu_transform_correction_map_free(TransformCorrectionMap correction_map);
+
+// TODO: Indexing set binding with verbose enum response
+
 }
 
 #endif
--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -1,17 +1,44 @@
 #![allow(clippy::missing_safety_doc)]
 
-use core::{ptr, slice};
-use spirv_webgpu_transform::{combimgsampsplitter, drefsplitter};
+use core::{ffi, ptr, slice};
+use spirv_webgpu_transform::{CorrectionMap, combimgsampsplitter, drefsplitter};
+
+type TransformCorrectionMap = *mut ffi::c_void;
+
+pub unsafe fn alloc_or_pass_correction_map(
+    map: *mut TransformCorrectionMap,
+) -> &'static mut Option<CorrectionMap> {
+    unsafe {
+        if map.is_null() {
+            panic!(
+                "Got null correction map pointer, pointer to existing correction map or SPIRV_WEBGPU_TRANFORM_CORRECTION_MAP_NULL"
+            )
+        }
+
+        if (*map).is_null() {
+            let owned = Box::new(None);
+            let r = Box::leak(owned);
+            let ptr = r as *mut Option<CorrectionMap> as TransformCorrectionMap;
+            *map = ptr;
+            r
+        } else {
+            Box::leak(Box::from_raw((*map).cast::<Option<CorrectionMap>>()))
+        }
+    }
+}
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn combimgsampsplitter_alloc(
+pub unsafe extern "C" fn spirv_webgpu_transform_combimgsampsplitter_alloc(
     in_spv: *const u32,
     in_count: u32,
     out_spv: *mut *const u32,
     out_count: *mut u32,
+    correction_map: *mut TransformCorrectionMap,
 ) {
+    let correction_map = unsafe { alloc_or_pass_correction_map(correction_map) };
+
     let in_spv = unsafe { slice::from_raw_parts(in_spv, in_count as usize) };
-    match combimgsampsplitter(in_spv) {
+    match combimgsampsplitter(in_spv, correction_map) {
         Ok(spv) => unsafe {
             *out_count = spv.len() as u32;
             let leaked = Box::leak(spv.into_boxed_slice());
@@ -25,19 +52,22 @@ pub unsafe extern "C" fn combimgsampsplitter_alloc(
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn combimgsampsplitter_free(out_spv: *mut u32) {
+pub unsafe extern "C" fn spirv_webgpu_transform_combimgsampsplitter_free(out_spv: *mut u32) {
     unsafe { drop(Box::from_raw(out_spv)) }
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn dreftexturesplitter_alloc(
+pub unsafe extern "C" fn spirv_webgpu_transform_drefsplitter_alloc(
     in_spv: *const u32,
     in_count: u32,
     out_spv: *mut *const u32,
     out_count: *mut u32,
+    correction_map: *mut TransformCorrectionMap,
 ) {
+    let correction_map = unsafe { alloc_or_pass_correction_map(correction_map) };
+
     let in_spv = unsafe { slice::from_raw_parts(in_spv, in_count as usize) };
-    match drefsplitter(in_spv) {
+    match drefsplitter(in_spv, correction_map) {
         Ok(spv) => unsafe {
             *out_count = spv.len() as u32;
             let leaked = Box::leak(spv.into_boxed_slice());
@@ -51,6 +81,13 @@ pub unsafe extern "C" fn dreftexturesplitter_alloc(
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn dreftexturesplitter_free(out_spv: *mut u32) {
+pub unsafe extern "C" fn spirv_webgpu_transform_drefsplitter_free(out_spv: *mut u32) {
     unsafe { drop(Box::from_raw(out_spv)) }
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn spirv_webgpu_transform_correction_map_free(
+    correction_map: TransformCorrectionMap,
+) {
+    let _ = unsafe { Box::from_raw(correction_map.cast::<Option<CorrectionMap>>()) };
 }
